e6050acf409d101e7ee2e47363d8cf7b0e2c548e
Leonardo Persan <leopso1990@gmail.com.br>
2025-01-30 12:46:12 -0300

    feat: api documentation

---
+++ app/Scramble/Support/ExceptionToResponseExtensions/AuthorizationExceptionToResponseExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Scramble\Support\ExceptionToResponseExtensions;
+
+use Dedoc\Scramble\Extensions\ExceptionToResponseExtension;
+use Dedoc\Scramble\Support\Type\Type;
+
+class AuthorizationExceptionToResponseExtension extends ExceptionToResponseExtension
+{
+    public function shouldHandle(Type $type)
+    {
+        return false;
+    }
+}
